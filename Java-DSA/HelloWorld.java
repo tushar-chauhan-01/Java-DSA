319dc769ad39553d72441bf47bee20b7a19d31be
TUSHAR <82638641+tushar-chauhan-01@users.noreply.github.com>
2024-01-06 01:53:30 +0530

    Add files via upload

---
+++ Java-DSA/HelloWorld.java
@@ -0,0 +1,15 @@
+/* 
+THIS IS HOW TO USE MULTI LINE COMMENT
+THIS CODE IS HELLO WORLD CODE
+*/
+class HelloWorld{
+    public static void main(String args[]){
+        // PRINT - PRINTS WITHOUT LINE BREAK
+        System.out.print("'PRINT' WILL PRINT THINGS IN SINGLE LINE - ");
+        System.out.print("LIKE THIS");
+        // PRINTLN - PRINTS WITH LINE BREAK
+        System.out.println(" ## ");
+        System.out.println("'PRINTLN' WILL PRINT THINGS IN NEW LINE EVERYTIME - ");
+        System.out.println("LIKE THIS");
+    }
+}
---
+++ Java-DSA/PATTERNS/NxN_star.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class NxN_star
+{
+	public static void main(String args[]) {
+
+        System.out.println("PLEASE ENTER VALUE OF 'N' --> ");
+		Scanner st = new Scanner(System.in);
+		int n = st.nextInt();
+
+		for ( int i=0; i<n; i++ ){
+			for ( int j=0; j<n; j++ ){
+				System.out.print("#");
+			}
+			System.out.println(); 
+		}
+		 
+	}
+}

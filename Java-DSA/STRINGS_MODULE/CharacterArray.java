d9b282f2248c02b020c528b068cd7e35b0599d4d
TUSHAR <82638641+tushar-chauhan-01@users.noreply.github.com>
2024-02-12 22:50:29 +0530

    Add files via upload

---
+++ Java-DSA/STRINGS_MODULE/CharacterArray.java
@@ -0,0 +1,11 @@
+public class CharacterArray {
+    public static void main(String[] args) {
+        char arr[] = {'a', 'b', 'c', 'd'};
+
+        System.out.println(arr); // prints values
+
+        System.out.println(arr.toString()); // prints memory location
+
+        System.out.println(Character.toUpperCase(arr[0])); // convert char to uppercase
+    }
+}

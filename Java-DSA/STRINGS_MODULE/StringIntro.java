7e0f71735c93e86de8740884512a44e579820d44
TUSHAR <82638641+tushar-chauhan-01@users.noreply.github.com>
2024-02-16 19:51:58 +0530

    Add files via upload

--- Java-DSA/STRINGS_MODULE/StringIntro.java
+++ Java-DSA/STRINGS_MODULE/StringIntro.java
@@ -1,13 +1,28 @@
+
 public class StringIntro {
     public static void main(String[] args) {
         // string internally is stored in an array format only and are immutable aka cant be changed
+        
         // "STRING" is basically a wrapper around a character array
+
+        // direct assignment
         String s1 = "Java";
         String s2 = "Programming";
 
         System.out.println(s1.toLowerCase());
         System.out.println(s2.toUpperCase());
-
         System.out.println(s1.charAt(2));
+
+        // Passing string literal
+        String s3 = new String("Coding");
+        System.out.println(s3);
+
+        // Passing charachter array
+        char lst[] = {'a','b','c','d','e','f','g'};
+        System.out.println(lst);
+
+        // Passing byte array
+        char lst1[] = {97,98,99,100,101};
+        System.out.println(lst1);
     }
 }
